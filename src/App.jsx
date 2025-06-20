bf0a17c2e0ca2a3944d6744db72dc7041c1bc3cb
eber2k1 <lit27k@gmail.com>
2025-06-20 15:59:47 -0500

    update enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from "./pages/home"
-import { PokemonPage } from './pages/PokemonPage';
+import Home from './pages/home';
+import PokemonPage from './pages/PokemonPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/pokedemo">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pokemon/:id" element={<PokemonPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
